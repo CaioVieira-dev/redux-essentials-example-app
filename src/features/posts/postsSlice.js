f0b21f25fef4163b880d1c9c65f4d1de161cc36c
Caio Vieira <caiocesarav0208@gmail.com>
2021-08-23 08:40:40 -0300

    feat: :sparkles: create postsSlice

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,14 @@
+import {createSlice} from  '@reduxjs/toolkit'
+
+const initialState = [
+    {id: 1,title: 'First post', content: 'hello'},
+    {id: 2,title: 'Second post', content: 'more text'},
+]
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState: initialState,
+    reducers: {}
+})
+
+export default postsSlice.reducer
