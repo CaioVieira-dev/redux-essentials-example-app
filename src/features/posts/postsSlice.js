1de9c4c5056c4d706852400cfc94738698f0365c
Caio Vieira <caiocesarav0208@gmail.com>
2021-08-23 16:00:32 -0300

    feat: :sparkles: get data from fakeApi
    use thunks to get async data from fake api

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,20 +1,9 @@
 
 import {createSlice, nanoid, createAsyncThunk} from  '@reduxjs/toolkit'
 import {client} from '../../api/client'
-import {sub} from 'date-fns'
 
 const initialState = {
-    posts:[
-    {id: 1,title: 'First post', 
-    content: 'hello',
-    reactions:{ thumbsUp: 10,  hooray: 0,  heart: 0,  rocket: 0,  eyes: 0},
-    date:sub(new Date(),{minutes:10}).toISOString()},
-    {id: 2,
-    title: 'Second post', 
-    content: 'more text',
-    reactions:{ thumbsUp: 0,  hooray: 10,  heart: 0,  rocket: 0,  eyes: 0},
-    date:sub(new Date(),{minutes:5}).toISOString()},
-],
+    posts:[],
 status:'idle',
 error:null
 }
@@ -64,6 +53,7 @@ const postsSlice = createSlice({
         },
         [fetchPosts.fulfilled]:(state,action)=>{
             state.status = 'succeeded'
+            state.posts = state.posts.concat(action.payload)
         },
         [fetchPosts.rejected]:(state,action) =>{
             state.status = 'failed'
