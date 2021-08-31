01288f62d082e459ed0a77f2049775bd7f5e5b67
Caio Vieira <caiocesarav0208@gmail.com>
2021-08-31 08:22:30 -0300

    feat: :sparkles: add custom logger middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore} from '@reduxjs/toolkit'
 
 import postsReducer from '../features/posts/postsSlice'
 import usersReducer from '../features/users/usersSlice'
 import notificationsReducer from '../features/notifications/notificationsSlice'
+
+const myLoggerMiddleware = storeAPI => next => action => {  
+  console.log('Dispatching: ',action)
+  console.time('dispatch')
+  const result = next(action)
+  console.log('Dispatch done in:')
+  console.timeEnd('dispatch')
+  console.log('Result: ',result)
+  return result
+}
 
 export default configureStore({
   reducer: {
@@ -10,4 +20,5 @@ export default configureStore({
     users:usersReducer,
     notifications:notificationsReducer,
   },
+  middleware:(getDefaultMiddleware)=>getDefaultMiddleware().prepend(myLoggerMiddleware)
 })
